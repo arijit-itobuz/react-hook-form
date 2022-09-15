90acb39d6ae961011dbab5c9962599d295e0d598
Arijit Das <arijit@itobuz.com>
2022-09-15 11:44:38 +0530

    feat: update

--- src/Component/Routes/AppRoutes.jsx
+++ src/Component/Routes/AppRoutes.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import UseForm from '../Form/UseForm';
+import UseForm from '../Form/Form';
 import UseFieldArray from '../Form/UseFieldArray';
 
 export default function AppRoutes() {
   return (
     <>
       <Routes>
+        <Route path='/' element={<><h1>Home</h1></>} />
         <Route path='/useForm' element={<UseForm />} />
         <Route path='/useFieldArray' element={<UseFieldArray />} />
       </Routes>
